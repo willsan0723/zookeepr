b23c7d49d6ad446a95017d05f51eaadaad24831d
willsan0723 <william.santee@gmail.com>
2020-10-08 17:42:46 -0600

    finished frontend code

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 
 
@@ -109,6 +110,18 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(req.animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
