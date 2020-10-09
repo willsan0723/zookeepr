b8e8dde6759dbf751763eafe0d30c277bca0f5c4
willsan0723 <william.santee@gmail.com>
2020-10-09 13:12:04 -0600

    finished with zookepers page

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,4 +1,5 @@
 const $displayArea = document.querySelector('#display-area');
+const $zookeeperForm = document.querySelector("#zookeeper-form");
 
 const printResults = resultArr => {
   console.log(resultArr);
@@ -40,5 +41,17 @@ const getZookeepers = (formData = {}) => {
     });
 };
 
+handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
 
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+}
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
