84aa1cc96065d8a3da340d6321850d4969856662
willsan0723 <william.santee@gmail.com>
2020-10-08 18:16:03 -0600

    forgot to create new branch

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,12 @@
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
